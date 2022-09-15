1bf060e014ecbdf268e268976368e078a9688e0c
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2022-09-15 21:55:52 +0200

    Assignment 2 finished

--- src/Program.java
+++ src/Program.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Program {
 
-    static void main(){
+    public static void main(String[] args){
         Program myProgram = new Program();
         myProgram.start();
 
@@ -10,6 +10,48 @@ public class Program {
 
     void start()
     {
+        String courseName;
+        Integer numberOfStudents;
+        Double gradeAverage;
+        String bestStudent = "";
+        Scanner scanner = new Scanner(System.in);
 
-    }
+        System.out.print("Enter course name: ");
+        courseName = scanner.nextLine();
+        System.out.print("Enter number of students: ");
+        numberOfStudents = scanner.nextInt();
+
+        String[] studentNames = new String[numberOfStudents];
+        Integer[] studentGrades = new Integer[numberOfStudents];
+
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            System.out.print("Enter name of student " + (i+1) + ": ");
+            studentNames[i] = scanner.next();
+        }
+
+        System.out.println("");
+
+        int gradesTotal = 0;
+        int bestGrade = 0;
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            System.out.print("Enter the " + courseName + " grade of student " + studentNames[i] + ": ");
+            studentGrades[i] = scanner.nextInt();
+
+            gradesTotal = gradesTotal + studentGrades[i];
+
+            if (studentGrades[i] > bestGrade)
+            {
+                bestStudent = studentNames[i];
+            }
+        }
+
+        scanner.close();
+        gradeAverage = (double)gradesTotal / numberOfStudents;
+
+        System.out.println("");
+        System.out.println("Average grade: " + gradeAverage);
+        System.out.println("Best student in the course " + courseName + ": " + bestStudent);
+        }
 }

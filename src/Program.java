bfa4c2d3a9ac39b0f09cbb655fcf9cc704082d7d
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2022-09-15 19:45:17 +0200

    Program class

---
+++ src/Program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Program {
+
+    static void main(){
+        Program myProgram = new Program();
+        myProgram.start();
+
+    }
+
+    void start()
+    {
+
+    }
+}

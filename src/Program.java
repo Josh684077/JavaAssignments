4909d88126bb56cd0cb428b564422e190d3ce3df
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2022-09-16 16:28:38 +0200

    assignment3 finished

--- src/Program.java
+++ src/Program.java
@@ -16,5 +16,23 @@ public class Program {
         System.out.println("Please enter the size of your group and press [ENTER]");
         Integer numberOfStudents = scanner.nextInt();
 
+        Student[] students = new Student[numberOfStudents];
+
+        for (Integer i = 0; i < numberOfStudents; i++)
+        {
+            students[i] = new Student();
+            System.out.println("Please enter the name of student " + (i + 1) + " and press [ENTER]");
+            students[i].name = scanner.next();
+            System.out.println("Please enter Y if " + students[i].name + " is present and N if they are not, and press [ENTER]");
+            String answer = scanner.next();
+            students[i].isPresent = (answer.equalsIgnoreCase("Y"));
+        }
+
+        System.out.println("Student name: | Is present:");
+        for (Integer i = 0; i < numberOfStudents; i++)
+        {
+            System.out.println(students[i].name + " | " + students[i].isPresent);
+        }
+
     }
 }

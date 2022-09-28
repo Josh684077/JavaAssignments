cb87171caa4e73493adb11d0edaec59627af442c
Joshua Andrea <joshua.andrea@hotmail.com>
2022-09-28 14:36:47 +0200

    database class

---
+++ src/Database.java
@@ -0,0 +1,23 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database
+{
+    private List<Account> accountList = new ArrayList<Account>();
+
+    public void loginStudent(String username, String password)
+    {
+
+    }
+
+    public void initiateAccounts()
+    {
+
+    }
+
+    public void addAccount()
+    {
+
+    }
+}
